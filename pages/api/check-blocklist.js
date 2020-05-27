9a9ce4eb87642a281809d25f891a8e0d70bec627
Alex Madrzyk <amadrzyk@slack-corp.com>
2020-05-27 13:03:16 -0700

    add cors

--- pages/api/check-blocklist.js
+++ pages/api/check-blocklist.js
@@ -1,9 +1,28 @@
 import axios from 'axios'
+import Cors from 'cors'
 
 const BLOCKLIST_ENDPOINT = 'https://blocklist.cyberthreatcoalition.org/vetted/domain.txt';
 
+const cors = Cors({
+	methods: ['GET', 'HEAD'],
+});
+
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result)
+			}
+
+			return resolve(result)
+		})
+	})
+}
+
 export default async (req, res) => {
 	try {
+		await runMiddleware(req, res, cors);
+
 		const response = {
 			safe: [],
 			unsafe: []
