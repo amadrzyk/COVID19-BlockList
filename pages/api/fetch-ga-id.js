92717cccc40a18f3a6efebbddc135e5bf0280e6f
Alex Madrzyk <amadrzyk@slack-corp.com>
2020-05-27 12:48:00 -0700

    minor update

--- pages/api/fetch-ga-id.js
+++ pages/api/fetch-ga-id.js
@@ -1,8 +1,3 @@
-// import environment variables in dev
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 export default async (req, res) => {
     return res.json({
         id: process.env.GA_TRACKING_ID
